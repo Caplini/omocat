d2f44bdf57eb07932a3f8411863229c219b65e4b
Caplini <kylenoodles777@Gmail.com>
2023-10-05 23:35:27 +0100

    statistics

--- script.js
+++ script.js
@@ -55,7 +55,11 @@ window.updateAgeDisplay = function() {
     const ageElement = document.getElementById('age');
     const age = calculateAge();
     ageElement.textContent = age.toFixed(9);
+
+    // Update statistics
+    updateStatistics();
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     if(!localStorage.getItem("birthDate")) {
@@ -69,6 +73,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Update the age display immediately
         updateAgeDisplay();
+        updateStatistics();
+
 
         // Update frequently for a smooth flow
         setInterval(updateAgeDisplay, 10);
@@ -84,3 +90,66 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = window.location.href.split('?')[0];
     }
 });
+
+function openTab(tabName, elmnt) {
+    let i, tabcontent, tablinks;
+    let ageText = document.getElementById('text');
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablink" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    elmnt.classList.add("active");
+
+    // Hide the "AGE" text if Statistics tab is loaded, otherwise show it.
+    if(tabName === 'statistics') {
+        ageText.style.display = "none";
+    } else {
+        ageText.style.display = "block";
+    }
+}
+
+function updateStatistics() {
+    const ageInYears = calculateAge();
+    
+    const averageLifespan = 80;
+    const lifeLeft = averageLifespan - ageInYears;
+    const lifePercentageLived = (ageInYears / averageLifespan) * 100;
+
+    const hoursPerDay = 24;
+    const daysPerYear = 365.25; // considering leap years
+
+    const sleepHoursPerDay = 7;
+    const sleepTime = sleepHoursPerDay * ageInYears * daysPerYear;
+    const sleepPercentage = (sleepTime / (ageInYears * hoursPerDay * daysPerYear)) * 100;
+
+    const eatHoursPerDay = 1;
+    const eatingTime = eatHoursPerDay * ageInYears * daysPerYear;
+    const eatingPercentage = (eatingTime / (ageInYears * hoursPerDay * daysPerYear)) * 100;
+
+    const workHoursPerDay = 8;
+    const workYears = Math.min(ageInYears, 40); // assuming work/school for max 40 years
+    const workTime = workHoursPerDay * workYears * daysPerYear;
+    const workPercentage = (workTime / (ageInYears * hoursPerDay * daysPerYear)) * 100;
+
+    document.getElementById("lifeLeft").textContent = lifeLeft.toFixed(2);
+    document.getElementById("sleepTime").textContent = sleepTime.toFixed(2) + " hours (" + sleepPercentage.toFixed(2) + "% of your life)";
+    document.getElementById("eatingTime").textContent = eatingTime.toFixed(2) + " hours (" + eatingPercentage.toFixed(2) + "% of your life)";
+    document.getElementById("workTime").textContent = workTime.toFixed(2) + " hours (" + workPercentage.toFixed(2) + "% of your life)";
+    
+    // Assuming there's an element in your HTML with ID 'percentageLived' to display the percentage of life lived.
+    document.querySelector("#percentageLived span").textContent = lifePercentageLived.toFixed(2) + "%";
+}
+
+
+
