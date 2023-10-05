f9a9446318aac43a50e6c3e39bb41ed42918ce63
Caplini <kylenoodles777@Gmail.com>
2023-10-05 22:11:37 +0100

    age calcualtor upidates

--- age/script.js
+++ age/script.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Birthdate details
-    const birthYear = 2005;
-    const birthMonth = 11; // Note: Months in JavaScript are 0-indexed. So, January is 0 and December is 11.
-    const birthDay = 13;
-    const birthHour = 16;  // 4pm
 
-    function calculateAge() {
+    let birthYear, birthMonth, birthDay, birthHour;
+
+    if(!localStorage.getItem("birthDate")) {
+        birthYear = prompt("Enter your birth year:");
+        birthMonth = prompt("Enter your birth month (1-12):");
+        birthDay = prompt("Enter your birth day (1-31):");
+        birthHour = prompt("Enter your birth hour (0-23):");
+
+        localStorage.setItem("birthDate", JSON.stringify({ year: birthYear, month: birthMonth, day: birthDay, hour: birthHour }));
+    } else {
+        const storedData = JSON.parse(localStorage.getItem("birthDate"));
+        birthYear = storedData.year;
+        birthMonth = storedData.month;
+        birthDay = storedData.day;
+        birthHour = storedData.hour;
+    }
+
+    function calculateAge(format = 'years') {
         const birthDate = new Date(birthYear, birthMonth - 1, birthDay, birthHour);
         const now = new Date();
         const ageInMilliseconds = now - birthDate;
@@ -15,22 +27,31 @@ document.addEventListener('DOMContentLoaded', function() {
         const ageInDays = ageInHours / 24;
         const ageInYears = ageInDays / 365.25; // Considering leap years
 
-        return ageInYears;
+        switch(format) {
+            case 'years':
+                return ageInYears;
+            case 'days':
+                return ageInDays;
+            case 'hours':
+                return ageInHours;
+            case 'minutes':
+                return ageInMinutes;
+            case 'seconds':
+                return ageInSeconds;
+        }
     }
 
-    function updateAgeDisplay() {
+    window.updateAgeDisplay = function() {
         const ageElement = document.getElementById('age');
-        const age = calculateAge();
+        const timeFormat = document.getElementById('timeFormat').value;
+        const age = calculateAge(timeFormat);
 
-        // Format age with 9 decimal places
-        const formattedAge = age.toFixed(9);
-
-        ageElement.textContent = `You are ${formattedAge} years old.`;
+        ageElement.textContent = age.toFixed(9);
     }
 
     // Update the age display immediately
     updateAgeDisplay();
 
-    // Update every second (to keep the decimals changing)
-    setInterval(updateAgeDisplay, 1000);
+    // Update frequently for a smooth flow
+    setInterval(updateAgeDisplay, 10);
 });
